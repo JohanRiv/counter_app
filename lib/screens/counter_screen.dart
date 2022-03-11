2a8b797bce7ef36ab70a8f61e00808457f8c869b
JohanRiv <johancamiloandres@hotmail.com>
2022-03-10 23:07:16 -0500

    Convert to statefull widget the home screen

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    int counter = 0;
     const fontSize25 = TextStyle(fontSize: 25);
     return Scaffold(
       appBar: AppBar(
@@ -27,7 +33,8 @@ class HomeScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          counter += 1;
+          counter++;
+          setState(() {});
         },
         child: const Icon(Icons.add),
       ),
